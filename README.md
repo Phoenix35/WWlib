f538862c6cc306668df649479add71b7d6dea213
Phoenix35 <zak_bruno35@hotmail.fr>
2012-05-01 03:21:43 +0200

    Launching project. You can work with it, as with any Web Workers. Nothing special for the moment.
    Signed-off-by: Phoenix35 <zak_bruno35@hotmail.fr>

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 WWlib
 =====
 
+/*************************************\
+* Author: Phoenix35                   *
+* Year: 2012                          *
+* Released under GNU/GPL License      *
+*                                     *
+* Feel free to copy and use at will,  *
+* but please mention the author       *
+\*************************************/
+
+
 Some lazy Web Workers library
---
+++ js/wwlib.js
@@ -0,0 +1,55 @@
+(function (window) {
+
+var ww = (function() {
+
+var ww = function() {
+    return new ww.fn.init(arguments);
+}
+
+ww.fn = ww.prototype = {
+    constructor: ww,
+
+    FILE_CHECK: false,
+
+    init: function(scripts) {
+        for(var i in scripts)
+            if(scripts.hasOwnProperty(i))
+                this.add(scripts[i]);
+
+        return this;
+    },
+
+    add: function (scriptURI) {
+        if(typeof scriptURI !== "string")
+            throw new this.error(scriptURI+ " must be a string.");
+
+        // https://developer.mozilla.org/en/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
+        if(/^(?:data|javascript(?=:))/.test(scriptURI))
+            throw new this.error("Spec disallows data: and javascript: URLs");
+        
+        console.group();
+        console.log("Loading " +scriptURI);
+        var worker = new Worker(scriptURI);
+        // Creating one worker per script is not necessarily wanted
+        // TODO: Change -_-
+        console.log(scriptURI+ " downloaded.");
+        console.warn('It may have silently failed though, sorry!');
+        console.groupEnd();
+    },
+
+    error: function (message) {
+        this.name = "Web Workers Library Error";
+        this.message = message || "Unknown error";
+    }
+};
+
+return ww;
+
+})();
+
+ww.fn.init.prototype = ww.fn;
+
+
+window.ww = ww;
+
+})(window);
